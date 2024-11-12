25c790d270f0aac7f06538ab3b0f004fb101acc9
matthias <matthiaspetitpro76@gmail.com>
2024-11-12 17:22:27 +0100

    first commit

---
+++ DynamiserUnEspaceDeCommentaires_DocumentHTMLDevoir/DynamiserUnEspaceDeCommentaires_DocumentsDevoir/index.js
@@ -0,0 +1,24 @@
+function onclick() {
+  let commentlist = document.getElementById("comment-list");
+
+  let firstName = document.getElementById("first-name");
+
+  let lastName = document.getElementById("last-name");
+
+  let message = document.getElementById("message");
+
+  createHTMLElement = (div (class  flex space-x-4 text-sm text-gray-500))
+
+  message = document.createElement("p");
+
+  message = document.createTextNode(message);
+
+  firstName = document.createHTMLElement(
+    "div class=flex space-x-4 text-sm text-gray-500"
+  );
+  lastName = document.createElement("h3");
+
+  comment.appendChild(commentaire);
+
+  commentlist.appendChild(comment);
+}
